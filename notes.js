075294d95108d69c50a159c88cec781dd469f280
Peter Racine Jr <peterracinejr@gmail.com>
2021-05-27 14:19:44 -0500

    start: initial daily-notes .js

---
+++ notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    id: 1,
+    subject: "Learned how to code objects in arrays.",
+    date: "5/27/21",
+    feeling: "Feeling excited to be learning at NSS",
+    timeInMinutes: 240,
+  },
+
+  {
+    id: 2,
+    subject: "Learned how to use modules in .js",
+    date: "5/27/21",
+    feeling:
+      "Felt lost at first but got some great help from my classmates as well as Christina.",
+    timeInMinutes: 45,
+  },
+];
+
+console.log(notes);
